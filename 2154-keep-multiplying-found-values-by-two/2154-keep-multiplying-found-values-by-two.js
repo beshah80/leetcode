0b43015f14e07a6a9bedba6c00c1e90c84a7f874
Beshah <216701090+beshah80@users.noreply.github.com>
2025-11-27 15:55:52 +0300

    Time: 0 ms (100%), Space: 55.9 MB (33.35%) - LeetHub

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.js
@@ -0,0 +1,9 @@
+var findFinalValue = function(nums, original) {
+    const numSet = new Set(nums);
+    
+    while (numSet.has(original)) {
+        original *= 2;
+    }
+    
+    return original;
+};
